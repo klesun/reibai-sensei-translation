f6604d646dbe34c9d21e6b2960e091eaa3f1e13f
Artur ECMA God Klesun <arturklesun@gmail.com>
2022-02-13 16:16:43 +0200

    Split monouitic function a bit

--- public/index.ts
+++ public/index.ts
@@ -150,6 +150,75 @@ const getAllTranslatedWords = (bubbles: BubbleMapping, notes: NoteMapping) => {
 const URL_PARAM_PAGE = "pageIndex";
 const URL_PARAM_VOLUME = "volumeNumber";
 
+const focusBubbleSvg = (ocrIndex: number | string) => {
+    [...document.querySelectorAll('.focused-block-polygon')]
+        .forEach(poly => poly.classList.toggle('focused-block-polygon', false));
+    [...document.querySelectorAll('polygon[data-block-ocr-index="' + ocrIndex + '"]')]
+        .forEach(poly => poly.classList.toggle('focused-block-polygon', true));
+};
+
+const makeBubbleField = (
+    block: IndexedBlock,
+    qualifier: PageTransactionBase,
+    jpnToEng: Map<string, string>,
+    whenBubbleMapping: Promise<BubbleMapping>
+): HTMLElement => {
+    const jpnSentence = collectBlockText(block).trimEnd();
+    const engSentence = jpnToEng.get(jpnSentence);
+
+    const txBase = {
+        ...qualifier,
+        ocrBubbleIndex: block.ocrIndex,
+        jpn_ocr: jpnSentence,
+        bounds: getBlockBounds(block),
+    } as const;
+    let lastValue = '';
+    const textarea = Dom('textarea', {
+        type: 'text',
+        placeholder: engSentence || '', rows: 3,
+        'data-block-ocr-index': block.ocrIndex,
+        // TODO: check if blur triggers when you close browser
+        onblur: (evt: Event) => {
+            if (lastValue !== textarea.value) {
+                lastValue = textarea.value;
+                const tx: TranslationTransaction = {
+                    ...txBase,
+                    eng_human: lastValue,
+                    sentAt: new Date().toISOString(),
+                };
+                whenBubbleMapping.then(data => data.set(tx));
+            }
+        },
+    });
+    whenBubbleMapping.then(data => {
+        const lastTranslation = data.get(txBase);
+        if (lastTranslation) {
+            textarea.value = lastTranslation.eng_human + textarea.value;
+            lastValue = textarea.value;
+        }
+    });
+    return Dom('div', {
+        class: 'sentence-block',
+        onmousedown: () => focusBubbleSvg(block.ocrIndex),
+    }, [
+        Dom('div', {style: 'flex: 1'}),
+        Dom('div', {class: 'ocred-text-block'}, [
+            Dom('div', {}, jpnSentence),
+            Dom('div', {style: 'display: flex; align-items: end'}, [
+                Dom('div', {}, [
+                    Dom('div', {class: 'font-size-holder'}, getFontSize(block) + 'px'),
+                    Dom('div', {
+                        class: 'confidence-holder',
+                        title: 'Recognized Text Confidence'
+                    }, block.confidence.toFixed(2)),
+                ]),
+                Dom('div', {class: 'bubble-number-holder'}, '#' + block.ocrIndex),
+            ]),
+        ]),
+        textarea,
+    ]);
+};
+
 export default async (fetchingBubbles: Promise<Response>) => {
     const googleTranslationsPath = './unv/google_translations.json';
     const whenGoogleTranslations = fetch(googleTranslationsPath).then(rs => rs.json());
@@ -192,6 +261,7 @@ export default async (fetchingBubbles: Promise<Response>) => {
 
         const pageFileName = ('000' + pageIndex).slice(-3);
         const volumeDirName = ('00' + volumeNumber).slice(-2);
+        const qualifier = { volumeNumber, pageIndex };
 
         gui.current_page_img.setAttribute('src', "./unv/volumes/v" + volumeDirName + "/" + pageFileName + ".jpg");
 
@@ -234,74 +304,13 @@ export default async (fetchingBubbles: Promise<Response>) => {
             gui.annotations_svg_root.appendChild(polygon);
         }
 
-        const focusBubbleSvg = (ocrIndex: number | string) => {
-            [...document.querySelectorAll('.focused-block-polygon')]
-                .forEach(poly => poly.classList.toggle('focused-block-polygon', false));
-            [...document.querySelectorAll('polygon[data-block-ocr-index="' + ocrIndex + '"]')]
-                .forEach(poly => poly.classList.toggle('focused-block-polygon', true));
-        };
-
         gui.all_text_holder.innerHTML = '';
         const CELLS_PER_ROW = 3;
         // TODO: implement arrow navigation for bubbles on image
         for (const rowBlocks of chunked(blocks, CELLS_PER_ROW)) {
-            const blockCells = rowBlocks.map(block => {
-                const jpnSentence = collectBlockText(block).trimEnd();
-                const engSentence = jpnToEng.get(jpnSentence);
-
-                const txBase = {
-                    volumeNumber,
-                    pageIndex,
-                    ocrBubbleIndex: block.ocrIndex,
-                    jpn_ocr: jpnSentence,
-                    bounds: getBlockBounds(block),
-                } as const;
-                let lastValue = '';
-                const textarea = Dom('textarea', {
-                    type: 'text',
-                    placeholder: engSentence || '', rows: 3,
-                    'data-block-ocr-index': block.ocrIndex,
-                    // TODO: check if blur triggers when you close browser
-                    onblur: (evt: Event) => {
-                        if (lastValue !== textarea.value) {
-                            lastValue = textarea.value;
-                            const tx: TranslationTransaction = {
-                                ...txBase,
-                                eng_human: lastValue,
-                                sentAt: new Date().toISOString(),
-                            };
-                            whenBubbleMapping.then(data => data.set(tx));
-                        }
-                    },
-                });
-                whenBubbleMapping.then(data => {
-                    const lastTranslation = data.get(txBase);
-                    if (lastTranslation) {
-                        textarea.value = lastTranslation.eng_human + textarea.value;
-                        lastValue = textarea.value;
-                    }
-                });
-                return Dom('div', {
-                    class: 'sentence-block',
-                    onmousedown: () => focusBubbleSvg(block.ocrIndex),
-                }, [
-                    Dom('div', {style: 'flex: 1'}),
-                    Dom('div', {class: 'ocred-text-block'}, [
-                        Dom('div', {}, jpnSentence),
-                        Dom('div', {style: 'display: flex; align-items: end'}, [
-                            Dom('div', {}, [
-                                Dom('div', {class: 'font-size-holder'}, getFontSize(block) + 'px'),
-                                Dom('div', {
-                                    class: 'confidence-holder',
-                                    title: 'Recognized Text Confidence'
-                                }, block.confidence.toFixed(2)),
-                            ]),
-                            Dom('div', {class: 'bubble-number-holder'}, '#' + block.ocrIndex),
-                        ]),
-                    ]),
-                    textarea,
-                ]);
-            });
+            const blockCells = rowBlocks.map(block => makeBubbleField(
+                block, qualifier, jpnToEng, whenBubbleMapping
+            ));
             const remainderCells = [];
             for (let i = 0; i < CELLS_PER_ROW - blockCells.length; ++i) {
                 remainderCells.push(Dom('div', {class: 'remainder-cell'}));
