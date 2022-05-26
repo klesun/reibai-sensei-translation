18579641f25b5e82a25b52034ff4a3a0cb2220e6
klesun <arturklesun@gmail.com>
2022-05-26 09:15:00 +0300

    Fix wrong chapter matching

--- public/views/compile_images.ts
+++ public/views/compile_images.ts
@@ -52,6 +52,7 @@ export default async (
         for (let pageIndex = 0; pageIndex < pages; ++pageIndex) {
             const chapterNumber = chapters
                 .filter(c => c.startPage <= pageIndex + 1)
+                .slice(-1)
                 .map(c => c.chapter)[0] ?? 0;
             const qualifier = {volumeNumber, pageIndex};
             await CompileImage({qualifier, translations, gui});
